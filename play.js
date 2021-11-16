5066c4ea702a37b40e4e9ab2e6279903df3c7902
isissi <isissi_525@hotmail.com>
2021-11-15 19:31:11 -0700

    Added move commands and exit info

--- play.js
+++ play.js
@@ -1,4 +1,28 @@
 const connect = require('./client');
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding("utf8");
+
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+
+conn.on("connect", () => {
+
+  // console.log('Move: up');
+  stdin.on("data", (key) => {
+    if (key === "a") {
+      conn.write("Move: left");
+    } else if (key === "s") {
+      conn.write("Move: down");
+    } else if (key === "d") {
+      conn.write("Move: right");
+    } else if (key === "w") {
+      conn.write("Move: up");
+    }
+    if (key === "\u0003") {
+      console.log("Thanks for playing, ciao!");
+      process.exit();
+    }
+  });
+});
