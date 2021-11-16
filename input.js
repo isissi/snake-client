7c3830aa0745f7e183ed3ee21f9fe1c1443d5807
isissi <isissi_525@hotmail.com>
2021-11-15 20:48:37 -0700

    Added move commands

--- input.js
+++ input.js
@@ -3,16 +3,23 @@ const setupInput = function () {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on("data", handleUserInput);
   return stdin;
 };
-
 
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     console.log("Thanks for playing, ciao!");
     process.exit();
+  } else if (key === "a") {
+    conn.write("Move: left");
+  } else if (key === "s") {
+    conn.write("Move: down");
+  } else if (key === "d") {
+    conn.write("Move: right");
+  } else if (key === "w") {
+    conn.write("Move: up");
   }
-}
+};
 
 module.exports = setupInput;
