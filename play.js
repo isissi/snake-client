d817d05542e253a60f1de590edda44547ba19685
isissi <isissi_525@hotmail.com>
2021-11-15 20:48:59 -0700

    functions removed

--- play.js
+++ play.js
@@ -1,31 +1,7 @@
-const { stdin } = require('process');
-const connect = require('./client');
-const {setupInput} = require('./input');
-
-
+const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
-const conn = connect();
+connect();
+
 setupInput();
-
-
-
-
-
-conn.on("connect", () => {
-
-  // console.log('Move: up');
-  stdin.on("data", (key) => {
-    if (key === "a") {
-      conn.write("Move: left");
-    } else if (key === "s") {
-      conn.write("Move: down");
-    } else if (key === "d") {
-      conn.write("Move: right");
-    } else if (key === "w") {
-      conn.write("Move: up");
-    }
-    
-  });
-});
-
