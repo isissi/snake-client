2d93ce1c3f6e579de4d8d8aca542ebcc1a8a5651
isissi <isissi_525@hotmail.com>
2021-11-17 19:11:30 -0700

    code clean up

--- client.js
+++ client.js
@@ -1,18 +1,15 @@
 const net = require("net");
 const { IP, PORT } = require("./constants");
 
-// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: IP, // IP address here,
-    port: PORT, // PORT number here,
+    host: IP, 
+    port: PORT, 
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    // code that does something when the connection is first established
     console.log("Successfully connected to game server.");
     conn.write("Name: SX");
   });
